63b51579a240ae607f723f44d6103d8139675cf9
slitac <142998568+slitac@users.noreply.github.com>
2023-09-18 13:14:43 +0200

    Update _document.js
    google analytique

--- pages/_document.js
+++ pages/_document.js
@@ -28,6 +28,16 @@ class MyDocument extends Document {
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2596135861331646"
             crossorigin="anonymous"
           ></script>
+
+              <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-8FV6LTK6TS"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-8FV6LTK6TS');
+</script>
         </Head>
         <body>
         <Main />
